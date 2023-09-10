58610b49381cf81f49b0361e84daa1e5d6b532ae
Bowie <leonardo.lopes@estudante.ufscar.br>
2023-09-10 15:30:02 -0300

    Giving up on gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App/index.tsx'
-import { HashRouter, Routes, Route } from "react-router-dom";
 
 import './styles/globals.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter basename="homepage">
-      <Routes>
-        <Route path="/" Component={App} element={<App />}/>
-      </Routes>
-    </HashRouter>
         <App/>
   </React.StrictMode>
 )
